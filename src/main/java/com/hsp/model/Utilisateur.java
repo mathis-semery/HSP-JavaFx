5cc4db08563806148e6a0713b2e48fdf43505e20
Elias <zohrielias95@gmail.com>
2026-01-11 17:08:06 +0100

    Classe Utilisteur faite

--- src/main/java/com/hsp/model/Utilisateur.java
+++ src/main/java/com/hsp/model/Utilisateur.java
@@ -1,4 +1,85 @@
 package com.hsp.model;
 
 public class Utilisateur {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String mdp;
+    private int id;
+    private String role;
+
+    public Utilisateur(int id, String nom, String prenom, String email, String mdp, String role) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.mdp = mdp;
+        this.role = role;
+    }
+
+    public Utilisateur(String nom, String prenom, String email, String mdp, String role) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.mdp = mdp;
+        this.role = "ROLE_USER";
+    }
+
+    public Utilisateur(String email, String mdp) {
+        this.email = email;
+        this.mdp = mdp;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
