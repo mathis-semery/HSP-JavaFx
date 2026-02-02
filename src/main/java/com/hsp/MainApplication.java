77a3bbbdaa81b5998318b473fa4cf888b2241f5a
Mathis Semery <m.semery@lprs.fr>
2026-02-02 10:01:49 +0100

    [FEAT] Ajout de MainApplication.java seulement pour lancer l'appli

--- src/main/java/com/hsp/MainApplication.java
+++ src/main/java/com/hsp/MainApplication.java
@@ -1,4 +1,22 @@
 package com.hsp;
 
-public class MainApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/auth/Login.fxml"));
+        Scene scene = new Scene(loader.load(), 800, 600);
+        stage.setTitle("HSP Urgences");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,18 @@
 module com.hsp.hsp_urgences {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires java.sql;
     requires jbcrypt;
+
+    opens com.hsp to javafx.graphics;
+    opens com.hsp.controller.patient to javafx.fxml;
+    opens com.hsp.controller.dossier to javafx.fxml;
+    opens com.hsp.controller.hospitalisation to javafx.fxml;
+    opens com.hsp.controller.stock to javafx.fxml;
+
+    exports com.hsp;
 }
